d65c94d453deb7f3071d2c44b979a10e99faf3db
mlkylmx <melikeyilmaz04@icloud.com>
2025-11-17 20:17:35 +0300

    login.css and forgot.css updated

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Acer\OneDrive\Masaüstü\TODOAPP\venv"
+set "VIRTUAL_ENV=C:\Users\Acer\OneDrive\Masaüstü\TODO_LAB\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
