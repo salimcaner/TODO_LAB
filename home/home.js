da337fa7ef7e7ec249c4d185661e1a100b4b8f04
mlkylmx <melikeyilmaz04@icloud.com>
2025-11-17 23:04:57 +0300

    Home Page Update

---
+++ home/home.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.querySelector(".toggle-btn");
+    const body = document.body;
+
+
+    toggleButton.addEventListener('click', function() {
+        body.classList.toggle("sidebar-closed");
+    });
+});
